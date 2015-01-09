ff2fd52a258b3d2b23210548c3bde598d6e4b8b6
Mike Cronce <mike@quadra-tec.net>
2015-01-09 12:52:45 -0500

    deploy.sh:  rm $MODULI is now done via runcmd; IOW, it will not get removed during a dry run

--- deploy.sh
+++ deploy.sh
@@ -92,7 +92,7 @@ if [ "$SSHD_CONFIG" != '' ]; then
 
 	# If there's nothing left in the moduli file (or it didn't exist at all), we should populate it
 	if [ "$(stat -nf %z "$MODULI")" -lt 10 ]; then
-		rm "$MODULI";
+		runcmd rm "$MODULI";
 		runcmd ssh-keygen -T "$MODULI" -f <(ssh-keygen -q -G /dev/stdout -b 4096 2> >(while read line; do echo "+++ ${line}" > /dev/stderr; done));
 	fi;
 
